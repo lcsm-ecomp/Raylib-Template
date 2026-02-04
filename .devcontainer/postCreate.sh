48171d68ea0ee66e8d1e6513414a1e7413b17f8b
lcsm-ecomp <32273415+lcsm-ecomp@users.noreply.github.com>
2026-02-04 12:00:58 -0300

    Update postCreate.sh

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -7,7 +7,7 @@ sudo apt-get update
 sudo apt-get install -y --no-install-recommends   build-essential cmake ninja-build git pkg-config python3
 
 # Build inicial (Web) para ficar "pronto" ao abrir o Codespace
-./scripts/build-web.sh
+#./scripts/build-web.sh
 echo
 echo "OK! Web build concluído."
 echo "Para rodar: ./scripts/serve-web.sh  (abre na porta 8000)"
